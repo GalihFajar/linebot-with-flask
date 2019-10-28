201f901a3a131971deec037081e5b06d38d4a20f
Galih Fajar <galihfajar00@gmail.com>
2019-10-28 12:41:09 +0700

    Testing New Script"

--- app.py
+++ app.py
@@ -1,26 +1,10 @@
-# -*- coding: utf-8 -*-
-
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       https://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
-
-from __future__ import unicode_literals
 
 import os
 import sys
-from argparse import ArgumentParser
-
 from flask import Flask, request, abort
+
 from linebot import (
-    LineBotApi, WebhookParser
+    LineBotApi, WebhookHandler
 )
 from linebot.exceptions import (
     InvalidSignatureError
@@ -31,51 +15,36 @@ from linebot.models import (
 
 app = Flask(__name__)
 
-# get channel_secret and channel_access_token from your environment variable
-channel_secret = "YOUR_LINE_CHANNEL_SECRET"
-channel_access_token = "YOUR_LINE_CHANNEL_ACCESS_TOKEN"
-if channel_secret is None:
-    print('Specify LINE_CHANNEL_SECRET as environment variable.')
-    sys.exit(1)
-if channel_access_token is None:
-    print('Specify LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
-    sys.exit(1)
-
-line_bot_api = LineBotApi(channel_access_token)
-parser = WebhookParser(channel_secret)
+line_bot_api = LineBotApi('4M7OKL3bDlZC/dRpKMyXvp3DhGT2e3HBDO+lBDzbRkkHBG166ljOXMIldXImFp7OLuSqjAvA9FLAmMbdH0AqVMbcp0gtqTmq7ecdeOcuRJ4Omp3JYGpPDl1HH/tLr+Z8i5uj7I4xYRqjOl1w4aEnNgdB04t89/1O/w1cDnyilFU=')
+handler = WebhookHandler('d43ba340b3cb8752365fb2101ff77ebf')
 
 
 @app.route("/callback", methods=['POST'])
 def callback():
+    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
 
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
 
-    # parse webhook body
+    # handle webhook body
     try:
-        events = parser.parse(body, signature)
+        handler.handle(body, signature)
     except InvalidSignatureError:
+        print("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
-    # if event is MessageEvent and message is TextMessage, then echo text
-    for event in events:
-        if not isinstance(event, MessageEvent):
-            continue
-        if not isinstance(event.message, TextMessage):
-            continue
-
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
 
     return 'OK'
 
 
-if __name__ == "__main__":
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    line_bot_api.reply_message(
+        event.reply_token,
+        TextSendMessage(text="Don't Know")) #TextSendMessage(text=event.message.text))
 
+
+if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
